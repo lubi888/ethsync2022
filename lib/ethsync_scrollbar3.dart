34362100f66166d61317a2f48c5e0f8a8cd9ecb9
Linux Ubiquitous <lubi888@users.noreply.github.com>
2022-04-13 15:12:22 +0100

    double assets removed

--- lib/ethsync_scrollbar3.dart
+++ lib/ethsync_scrollbar3.dart
@@ -4,6 +4,7 @@ import './ethsync_text.dart';
 class Scrollbar3 extends StatelessWidget {
   // const Scrollbar3({ Key? key }) : super(key: key);
   final String _kAsset1 = 'assets/images/go-download-website.png';
+  final String _kAsset2 = 'assets/images/duIcon.png';
 
   @override
   Widget build(BuildContext context) {
@@ -50,6 +51,14 @@ class Scrollbar3 extends StatelessWidget {
               height: 100.0,
               width: 100.0,
             ),
+          ),
+          Container(
+            child: Image.asset(
+                // image: AssetImage('assets/images/duicon.png'),
+                _kAsset2,
+                semanticLabel: 'du Icon',
+                height: 100.0,
+                width: 100.0),
           )
         ],
       ),
